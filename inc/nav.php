ded7a3b2752b4a18d34e9df1baf7f3fca44d53b1
williamkeys <william.keys1@gmail.com>
2012-09-03 12:44:19 -0400

    Fixing nav

--- inc/nav.php
+++ inc/nav.php
@@ -1,9 +1,9 @@
 <ul id="navlist" onclick="return forceConfirm();">
-<li><a href="../release/hosts.php" >Hosts</a></li>
-<li><a href="../release/musicians.php" >Musicians</a></li>
-<li><a href="../release/events.php" >Events</a></li>
-<li><a href="../release/planner.php" >Event Planner</a></li>
-<li><a href="../release/hostevent.php">Host Notes/Tickets</a></li>
-<li><a href="../release/sendmail.php" >Send Mail</a></li>
-<li><a href="../release/report.php">Housing Reports</a></li>
+<li><a href="hosts.php" >Hosts</a></li>
+<li><a href="musicians.php" >Musicians</a></li>
+<li><a href="events.php" >Events</a></li>
+<li><a href="planner.php" >Event Planner</a></li>
+<li><a href="hostevent.php">Host Notes/Tickets</a></li>
+<li><a href="sendmail.php" >Send Mail</a></li>
+<li><a href="report.php">Housing Reports</a></li>
 </ul>
